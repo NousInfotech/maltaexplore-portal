d3220a8cca631f02366b6f5641ebe74edd9df9fa
Rasmi142 <rasmioff@gmail.com>
2025-07-07 02:13:34 +0530

    fixed

--- src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,5 @@
 import SignInViewPage from '@/features/auth/components/sign-in-view';
 
-interface PageProps {
-  isDark?: boolean;
-  onToggleTheme?: () => void;
-}
-
-export default function Page({ isDark, onToggleTheme }: PageProps) {
-  return <SignInViewPage isDark={isDark} onToggleTheme={onToggleTheme} />;
+export default function Page() {
+  return <SignInViewPage />;
 }
