ad46e415ada355a33186c1d465aabc85211ff01c
Siva-S92 <sivagraphics4fashion@gmail.com>
2025-07-09 23:25:49 +0530

    checking vercel error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -26,6 +26,7 @@
 // #####################################################################################################
 
 
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
